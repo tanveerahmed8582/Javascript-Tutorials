786b2086c56031b32a0ef17270bd26cf94f65d68
Tanveer Ahmed <tanveerahmed85820@gmail.com>
2024-07-28 12:23:33 +0530

    learning Async

--- Async/script.js
+++ Async/script.js
@@ -1,3 +1,15 @@
-setTimeout(function(){
+const sayTanveer = function(){
   console.log("Tanveer")
-}, 2000);
+}
+setTimeout(sayTanveer, 2000);
+
+const changeText = function(){
+  document.querySelector('h1').innerHTML = "Best Javascript Course"
+}
+ const changeMe = setTimeout(changeText, 4000)
+
+ document.querySelector('#stop').addEventListener('click', function(){
+  clearTimeout(changeMe)
+  console.log("STOPPED")
+ })
+
